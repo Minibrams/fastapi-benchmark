c5c080a417fab852572480b477106302e509eccb
Anders Brams <abra@ambolt.io>
2021-12-07 22:29:13 +0100

    Dropped redundant agent

--- stress_async_json.sh
+++ stress_async_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-env LOCUST_ENDPOINT=/async/json/ locust -f async_agent.py --host http://127.0.0.1:4242 --users 500 --spawn-rate 50
+env LOCUST_ENDPOINT=/async/json/ locust -f agent.py --host http://127.0.0.1:4242 --users 500 --spawn-rate 50
--- sync_agent.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, constant
-
-class ApiUser(HttpUser):
-    wait_time = constant(1)
-
-    @task
-    def health_check(self):
-        self.client.get('/sync/')
