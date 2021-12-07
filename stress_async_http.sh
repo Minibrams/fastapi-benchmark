5f2664e9214309ee57f51a1eac1d346c954cc3c5
Anders Brams <abra@ambolt.io>
2021-12-07 23:01:38 +0100

    Running locust headless

--- stress_async_http.sh
+++ stress_async_http.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-env LOCUST_ENDPOINT=/async/http/ locust -f agent.py --host http://127.0.0.1:4242 --users 500 --spawn-rate 50
+env LOCUST_ENDPOINT=/async/http/ locust -f agent.py \
+--host http://127.0.0.1:4242 \
+--users 500 \
+--spawn-rate 50 \
+--run-time 60s \
+--headless
+
--- stress_sync_json.sh
+++ stress_sync_json.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-env LOCUST_ENDPOINT=/sync/json/ locust -f agent.py --host http://127.0.0.1:4242 --users 500 --spawn-rate 50
+env LOCUST_ENDPOINT=/sync/json/ locust -f agent.py \
+--host http://127.0.0.1:4242 \
+--users 500 \
+--spawn-rate 50 \
+--run-time 60s \
+--headless
